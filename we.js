90c6b527b219ea9ee35cda69b1316e43a3f360d6
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2022-01-11 21:36:58 +0530

    Commit1

---
+++ we.js
@@ -0,0 +1,7 @@
+// document.write("External JS","<br>");
+// document.getElementById('th').innerHTML="This is a para";
+
+// var a1=4;
+// document.write(a1);
+
+var age = prompt("Tell me ur age");
---
+++ web.js
@@ -0,0 +1,4 @@
+document.write("External JS");
+document.getElementById('th').innerHTML="This is para";
+var a1=4;
+document.write(a1);
